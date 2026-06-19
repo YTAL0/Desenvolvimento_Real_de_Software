4ee906d6eb38d166a6d87caa5e2b6d9ca4151741
YTAL0 <ytalo122@hotmail.com>
2026-06-19 00:17:29 -0300

    Escrita

--- Capitulo_2/src/BankTransactionalAnalyzerSimple.java
+++ Capitulo_2/src/BankTransactionalAnalyzerSimple.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
@@ -12,15 +13,25 @@ import java.util.List;
 public class BankTransactionalAnalyzerSimple {
     private static final String RESOURCES = "Capitulo_2/src/";//guarda o caminho
 
-    public static void main() throws Exception{
+    public static void main(String[] args) throws Exception{
         final Path path = Paths.get(RESOURCES + "bank-data-simple.csv");//criando url do caminho
         final List<String> lines = Files.readAllLines(path);
         double total = 0;
-
+        int fck = 0;
         for(String line: lines){
-            String[] columns = line.split(",");
-            double amount = Double.parseDouble(columns[1]);//pego os valores do indice 1 da nossa
-            total+=amount;
+            if(line.isBlank() || line.contains("Resultado,")){
+                fck++;
+                continue;
+            }
+                String[] columns = line.split(",");
+                double amount = Double.parseDouble(columns[1]);//pego os valores do indice 1 da nossa
+                total += amount;
+
+        }
+
+        if(fck == 0){
+        String result = "\nResultado, " +Double.toString(total);
+        Files.writeString(path, result, StandardOpenOption.APPEND);
         }
 
         IO.println("Nosso valor total "+ total);
